72ad32b2ede5cd79cddccf91a57b1fbc1c1c6214
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-09-19 21:52:04 +0300

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 1259px;
+  align-items: start;
+  justify-content: start;
   margin: 0 auto;
   padding: 0 15px;
 `;
